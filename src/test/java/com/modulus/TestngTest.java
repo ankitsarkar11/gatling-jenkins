9e7be1d7b2cb2bdf1924eb147e99d4543cafc59d
Ankit Sarkar <sankit@intertrust.com>
2024-05-14 19:29:26 +0530

    added a testng test and gatling logger

---
+++ src/test/java/com/modulus/TestngTest.java
@@ -0,0 +1,11 @@
+package com.modulus;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestngTest {
+    @Test
+    public void helloWorld() {
+        Assert.assertEquals(1,1);
+    }
+}
